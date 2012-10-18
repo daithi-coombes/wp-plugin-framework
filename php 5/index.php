91dfac90ca44f9937f082bc67d7ee19df7f81198
daithi <daithi@daithi-laptop>
2012-10-18 12:48:14 +0100

    php5 directory structure

---
+++ php 5/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package wp-plugin-framework
+ */
+/*
+  Plugin Name: A WP Plugin Framework
+  Plugin URI: https://github.com/david-coombes/wp-plugin-framework
+  Description: Framework for seperating logic from front end code.
+  Version: 0.1
+  Author: Daithi Coombes
+  Author URI: http://david-coombes.com
+ */
+
